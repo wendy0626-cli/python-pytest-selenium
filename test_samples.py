13b3141ca37b11cd65fc612c4f248fc9478f32db
Wendy <wanyang0626@163.com>
2022-07-12 15:48:08 +0800

    pytest-selenium

---
+++ test_samples.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+__author__ = 'Guibin'
+
+# 导包
+from selenium import webdriver
+from time import sleep
+
+# 创建浏览器驱动对象, 以下为创建不同浏览器驱动对象
+#driver = webdriver.Chrome(executable_path=r'E:\PythonProject\pythonProject1\chromedriver_win32\chromedriver.exe')    # Chrome浏览器
+
+# driver = webdriver.Firefox()   # Firefox浏览器
+
+# driver = webdriver.Edge()      # Edge浏览器
+
+# driver = webdriver.Ie()        # Internet Explorer浏览器
+
+# driver = webdriver.Opera()     # Opera浏览器
+
+# driver = webdriver.PhantomJS()   # PhantomJS
+
+# 打开指定网址
+#driver.get('https://www.baidu.com')
+# 休眠3秒
+#sleep(3)
+# 关闭浏览器驱动对象
+#driver.quit()
